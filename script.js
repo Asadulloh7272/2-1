49df3355aff75be7171032d1f45faaa9ece02223
Asadulloh7272 <rustamovasadulloh707@gmail.com>
2025-05-02 15:02:52 +0300

    asd
    asd

---
+++ script.js
@@ -0,0 +1,20 @@
+const apiUrl = ""
+const languages = {
+    "en": "English"
+    "uz": "Uzbek"
+    "ru": "Russian"
+    "fr": "French"
+    "de": "German"
+    "es": "Spanish"
+    "zh": "Chinese"
+    "ar": "Arabic"
+};
+
+
+document.addEventListener('DOMContentLoaded', () =>{
+    const inputLang = document.getElementById('inputLang');
+    const outputLang = document.getElementById('outputLang');
+    Object.entries(languages).forEach([ConvolverNode, name]) => {
+        inputLang.innerHTML += `<option value='$(code)'`>
+    }
+})
